48be0c2e5a24c31676b72a650b3d208778f8929e
OmegaApproximasterEthereal2004 <sunnybunnies09@naver.com>
2025-11-22 03:30:29 +0900

    Refactor CustomGameProfile to wrap GameProfile

--- dough-skins/src/main/java/io/github/bakedlibs/dough/skins/CustomGameProfile.java
+++ dough-skins/src/main/java/io/github/bakedlibs/dough/skins/CustomGameProfile.java
@@ -8,6 +8,8 @@ import javax.annotation.Nullable;
 
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.profile.PlayerProfile;
+import org.bukkit.profile.PlayerTextures;
 
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
@@ -15,10 +17,8 @@ import com.mojang.authlib.properties.Property;
 import io.github.bakedlibs.dough.reflection.ReflectionUtils;
 import io.github.bakedlibs.dough.versions.MinecraftVersion;
 import io.github.bakedlibs.dough.versions.UnknownServerVersionException;
-import org.bukkit.profile.PlayerProfile;
-import org.bukkit.profile.PlayerTextures;
 
-public final class CustomGameProfile extends GameProfile {
+public final class CustomGameProfile {
 
     /**
      * The player name for this profile.
@@ -33,44 +33,51 @@ public final class CustomGameProfile extends GameProfile {
 
     private final URL skinUrl;
     private final String texture;
+    private final GameProfile handle;
 
-    CustomGameProfile(@Nonnull UUID uuid, @Nullable String texture, @Nonnull URL url) {
-        super(uuid, PLAYER_NAME);
+    public CustomGameProfile(@Nonnull UUID uuid, @Nullable String texture, @Nonnull URL url) {
         this.skinUrl = url;
         this.texture = texture;
+        this.handle = new GameProfile(uuid, PLAYER_NAME);
 
         if (texture != null) {
-            getProperties().put(PROPERTY_KEY, new Property(PROPERTY_KEY, texture));
+            this.handle.getProperties().put(PROPERTY_KEY, new Property(PROPERTY_KEY, texture));
         }
     }
 
-    void apply(@Nonnull SkullMeta meta) throws NoSuchFieldException, IllegalAccessException, UnknownServerVersionException {
+    public void apply(@Nonnull SkullMeta meta) throws NoSuchFieldException, IllegalAccessException, UnknownServerVersionException {
         // setOwnerProfile was added in 1.18, but getOwningPlayer throws a NullPointerException since 1.20.2
         if (MinecraftVersion.get().isAtLeast(MinecraftVersion.parse("1.20"))) {
-            PlayerProfile playerProfile = Bukkit.createPlayerProfile(this.getId(), PLAYER_NAME);
+            PlayerProfile playerProfile = Bukkit.createPlayerProfile(this.handle.getId(), PLAYER_NAME);
             PlayerTextures playerTextures = playerProfile.getTextures();
             playerTextures.setSkin(this.skinUrl);
             playerProfile.setTextures(playerTextures);
             meta.setOwnerProfile(playerProfile);
         } else {
             // Forces SkullMeta to properly deserialize and serialize the profile
-            ReflectionUtils.setFieldValue(meta, "profile", this);
+            ReflectionUtils.setFieldValue(meta, "profile", this.handle);
 
             meta.setOwningPlayer(meta.getOwningPlayer());
 
             // Now override the texture again
-            ReflectionUtils.setFieldValue(meta, "profile", this);
+            ReflectionUtils.setFieldValue(meta, "profile", this.handle);
         }
-
     }
 
     /**
-     * Get the base64 encoded texture from the underline GameProfile.
+     * Get the base64 encoded texture from the underlying GameProfile.
      *
      * @return the base64 encoded texture.
      */
     @Nullable
     public String getBase64Texture() {
         return this.texture;
+    }
+
+    /**
+     * Access to the wrapped GameProfile.
+     */
+    public GameProfile getHandle() {
+        return this.handle;
     }
 }
